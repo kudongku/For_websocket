e58f8f5c8db00136bc08945a8504faa1c49b136f
kudongku <kudongku@naver.com>
2024-05-05 12:47:50 +0900

    :tada: feat(global) : git init

---
+++ src/main/java/com/isprotocol/for_websocket/ForWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.isprotocol.for_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isprotocol/for_websocket/ForWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isprotocol.for_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
